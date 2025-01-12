106e322d47d5b441c321a047f57041cd07d6da5e
dk <hoseop.ahn@gmail.com>
2025-01-12 23:38:08 +0900

    Add functionality to save and retrieve SQL queries from config.toml

--- views/gh_dbquery.py
+++ views/gh_dbquery.py
@@ -16,14 +16,39 @@ def get_sql_engine():
 import toml
 from pathlib import Path
 
-config_path = Path(".streamlit", "config.toml") 
-with open(config_path, "r", encoding="utf-8") as f:
-    config_data = toml.load(f)
-    sql_rtn = config_data["database"]["query_15"]
+def save_query(query_name, query):
+    config_path = Path(".streamlit", "config.toml")
+    with open(config_path, "r", encoding="utf-8") as f:
+        config_data = toml.load(f)
+        config_data["database"][query_name] = query
 
-engine = get_sql_engine()
+    with open(config_path, "w", encoding="utf-8") as f:
+        toml.dump(config_data, f)
 
-df_rtn = pandas.read_sql(sql_rtn, engine)
+def get_query(query_name):
+    config_path = Path(".streamlit", "config.toml")
+    with open(config_path, "r", encoding="utf-8") as f:
+        config_data = toml.load(f)
+        return config_data["database"][query_name]
 
-st.text_area("SQL", value=sql_rtn,height=400)
-st.dataframe(df_rtn)
+query_name = "query_15"
+sql_rtn = st.text_area("SQL", value=get_query(query_name),height=400)
+df_rtn = None
+
+col1, col2, col3 = st.columns([20,3,3])
+
+with col1:
+    query_name = st.text_input("Query Name", value=query_name, key="query_name")
+
+with col2:
+    if st.button("Run Query"):
+        engine = get_sql_engine()
+        df_rtn = pandas.read_sql(sql_rtn, engine)
+
+with col3:
+    if st.button("Save Query"):
+        save_query(query_name, sql_rtn)
+
+
+if df_rtn is not None:
+    st.dataframe(df_rtn)
