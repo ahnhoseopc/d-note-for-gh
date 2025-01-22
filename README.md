cdfeb6b59d61377c3bed093139a3e1ea0d7a1ccc
dk <hoseop.ahn@gmail.com>
2025-01-22 20:23:50 +0900

    ghmh changes

---
+++ README.md
@@ -0,0 +1,16 @@
+# Intro
+
+# Setup
+
+1. Install vscode
+2. Install vscode packages: git package(s), language packages, data manipulating packages, code assist packages, etc.
+3. Install python
+4. Install MS VS Build Tools for cx_Oracle old version to connect to Oracle 8i
+   (Oracle Instance Client 10.2)
+4. Install python packages with requirements.txt
+   (oracledb, streamlit, etc. )
+5. Install ipython package for jupyter note
+6. Install oracle instant client (for below 10g, thick client is necessary)
+   (https://www.oracle.com/kr/database/technologies/instant-client.html)
+   10.2 for 8i / 11.2 for 9i / 12.1 for 10g
+
--- views/gh_dbquery.py
+++ views/gh_dbquery.py
@@ -16,8 +16,9 @@ st.markdown("<style>.stTextArea>div>div>textarea { font-family: 'Courier New'; }
 
 if "query_sql" not in st.session_state:
     query_list = config.get_query_list()
-    query_sql = config.get_query("query_01")
+    query_sql = config.get_query(query_list[0])
     st.session_state.query_list = query_list
+    st.session_state.query_sql = query_sql
 
 sql_rtn = st.text_area("SQL", key="query_sql", height=400)
 df_rtn = None
@@ -37,10 +38,29 @@ with col3:
 
 with col4:
     if st.button("Run Query"):
-        df_rtn = db.run_sql(sql_rtn)
+        st.session_state.df_rtn = db.run_sql(sql_rtn)
 
-st.write(st.session_state.query_list)
-st.write(st.session_state.query_name)
+col1, col2 = st.columns([2,2])
 
-if df_rtn is not None:
-    st.dataframe(df_rtn)
+def on_select():
+    with col2:
+        print(st.session_state.selected_row)
+        if "selection" in st.session_state.selected_row:
+            if "rows" in st.session_state.selected_row["selection"]:
+                if len(st.session_state.selected_row["selection"]["rows"]) > 0:
+                    rownum = st.session_state.selected_row["selection"]["rows"][0]
+                    dict = st.session_state.df_rtn.iloc[rownum]
+                    # if "odr03odrcmt" in dict:
+                    #     dict.odr03odrcmt = base.decode_rtf(dict.odr03odrcmt)
+                    decode_rtf = lambda x: base.decode_rtf(x) if type(x) == str and base.is_rtf_format(x) else x
+                    dict = dict.map(decode_rtf)
+
+                    # st.dataframe(dict, use_container_width=True)
+                    json_str = dict.to_json(orient="index")
+                    st.json(json_str)
+
+# from st_aggrid import AgGrid, GridOptionsBuilder
+
+if "df_rtn" in st.session_state and st.session_state.df_rtn is not None:
+    with col1:
+        st.dataframe(st.session_state.df_rtn, on_select=on_select, selection_mode="single-row", key="selected_row")
