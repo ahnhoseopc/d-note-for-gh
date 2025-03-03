c20c80c1e167ca2243ea9608c84779c4056933af
dk <hoseop.ahn@gmail.com>
2025-03-04 04:57:52 +0900

    Enhance SQL query management interface by adding delete and new query functionalities, and improving layout for better usability

--- views/gh_dbquery.py
+++ views/gh_dbquery.py
@@ -5,45 +5,62 @@ import utils.db as db
 
 import streamlit as st
 
-def new_query():
-    st.session_state.query_name = "query_" + base.get_random_string(4)
-    st.session_state.query_sql = ""
-    st.experimental_rerun() # Force a re-render to update the selectbox
-
 def on_change_query_name():
     if "query_name" in st.session_state:
         st.session_state.query_sql = config.get_query(st.session_state.query_name)
 
 @auth.login_required
 def main():
-    st.markdown("<style>.stTextArea>div>div>textarea { font-family: 'Courier New'; }</style>", unsafe_allow_html=True)
+    #
+    # Text Area의 코드를 Courier New 로 설정하기
+    # 
+    st.markdown("<style>.stTextArea>div>div>textarea { font-family: 'Courier New'; font-size: 14px; }</style>", unsafe_allow_html=True)
 
     if "query_sql" not in st.session_state:
         query_list = config.get_query_list()
         query_sql = config.get_query(query_list[0])
         st.session_state.query_list = query_list
         st.session_state.query_sql = query_sql
+        st.session_state.query_name = query_list[0]
 
-    sql_rtn = st.text_area("SQL", key="query_sql", height=400)
-    df_rtn = None
+    col_select_name, col_del, col_new_name, col_new, col_save, col_run = st.columns([8,2,4,2,2,2])
 
-    col1, col2, col3, col4 = st.columns([16,2,2,2])
+    with col_del:
+        if st.button("Delete", use_container_width=True):
+            query_name = st.session_state.query_name
+            config.delete_query(query_name)
+            st.session_state.query_list.remove(query_name)
+            st.session_state.query_name = st.session_state.query_list[0]
+            st.session_state.query_sql = config.get_query(st.session_state.query_name)
 
-    with col1:
-        query_name = st.selectbox("Query Name", options=st.session_state.query_list, key="query_name", on_change=on_change_query_name, label_visibility="collapsed")
+    with col_new:
+        if st.button("New", use_container_width=True) and st.session_state.new_query_name:
+            st.session_state.query_sql = "-- COPIED FROM PREVIOUS QUERY\n" + st.session_state.query_sql
+            config.save_query(st.session_state.new_query_name, st.session_state.query_sql)
+            st.session_state.query_list += [st.session_state.new_query_name]
+            st.session_state.query_name = st.session_state.new_query_name
+            st.session_state.new_query_name = ""
 
-    with col2:
-        if st.button("New Query"):
-            new_query()
+    with col_select_name:
+        query_name = st.selectbox("Query Name", options=st.session_state.query_list, format_func=lambda x: x.upper(), key="query_name", on_change=on_change_query_name, label_visibility="collapsed")
 
-    with col3:
-        if st.button("Save Query"):
-            config.save_query(query_name, sql_rtn)
+    with col_new_name:
+        st.text_input("New Query Name", key="new_query_name",label_visibility="collapsed")
 
-    with col4:
-        if st.button("Run Query"):
-            st.session_state.df_rtn = db.run_sql(sql_rtn)
+    with col_save:
+        if st.button("Save", use_container_width=True):
+            config.save_query(query_name, st.session_state.sql_rtn)
 
+    with col_run:
+        if st.button("Run", use_container_width=True):
+            st.session_state.df_rtn = db.run_sql(st.session_state.sql_rtn)
+
+    # SQL
+    st.text_area("SQL", value=st.session_state.query_sql, key="sql_rtn", height=400)
+
+    #
+    # 결과 데이터프레임 - 건별 데이터
+    #
     if "df_rtn" in st.session_state and st.session_state.df_rtn is not None:
         col1, col2 = st.columns([2,2])
         with col1:
